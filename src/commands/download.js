972acff175e557ca03d74d8a591578592ed08c9c
massi <massi.mez@ya.ru>
2025-01-13 18:01:35 +0300

    remove throw error

--- src/commands/download.js
+++ src/commands/download.js
@@ -113,7 +113,6 @@ async function checkUserSubscription(bot, chatId, userId) {
     chatMember = await bot.getChatMember(CHANNEL_USERNAME, userId);
   } catch (error) {
     console.error("Error checking user subscription:", error);
-    throw new Error("Ошибка проверки подписки пользователя");
   }
   if (!["member", "administrator", "creator"].includes(chatMember.status)) {
     throw new Error(ERROR_MESSAGES.notSubscribed);
